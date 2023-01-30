44e4fc827f6141a5698b448d44ef289cc1a3bea2
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-30 12:40:38 -0800

    set up BrowserRouter and route /auth and /todos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Header from './components/Header/Header.js';
 import ToDos from './components/ToDos/ToDos.js';
+import Auth from './components/Auth/Auth.js';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <ToDos />
-    </>
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/todos" component={ToDos} />
+      </Switch>
+    </div>
   );
 }
 
