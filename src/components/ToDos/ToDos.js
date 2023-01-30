b27906ae3cdd361bf4722b0a2f370a31e9b06d02
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-30 12:19:38 -0800

    basic file structure and header and ToDos component setup

---
+++ src/components/ToDos/ToDos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ToDos.css';
+
+export default function ToDos() {
+  return (
+    <div className="to-dos">
+      <h2>ToDos placeholder</h2>
+    </div>
+  );
+}
