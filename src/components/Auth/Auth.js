59164b2ae4461429773fc31ddb6be95ddf9f7aa9
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-30 15:22:39 -0800

    add auth.js and set up auth form elements

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Auth.css';
 
 export default function Auth() {
-  return <div className="auth">Auth</div>;
+  //return navlinks for sign-in & sign-up, inputs for email & address, submit button
+  return (
+    <div className="auth">
+      <div className="form-container">
+        <p>To Dos Sign In</p>
+        <div>
+          <NavLink to="/auth/sign-in">Sign in</NavLink>
+          <NavLink to="/auth/sign-up">Sign up</NavLink>
+        </div>
+        <div>
+          <div>
+            <label>Email</label>
+            <input></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input></input>
+          </div>
+          {/* //on click call submitAuth() */}
+          <button>Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
