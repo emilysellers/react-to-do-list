01303073b46f7eef70f04a01a3c5a57941aac686
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-31 15:43:18 -0800

    authenticated users can see their to do list

--- src/context/ToDoContext.js
+++ src/context/ToDoContext.js
@@ -1,9 +1,25 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { getToDos } from '../services/todos.js';
+import { useUser } from './UserContext.js';
 
 const ToDoContext = createContext();
 
 const ToDoProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
+  const { user } = useUser();
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const data = await getToDos();
+        setTasks(data);
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+    fetchItems();
+  }, [user]);
+
   return <ToDoContext.Provider value={{ tasks, setTasks }}>{children}</ToDoContext.Provider>;
 };
 
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,5 +1,10 @@
 import { checkError, client } from './client.js';
 
+export async function getToDos() {
+  const response = await client.from('todos').select();
+  return checkError(response);
+}
+
 export async function createToDo(description) {
   const response = await client.from('todos').insert([{ description }]).single();
   return checkError(response);
