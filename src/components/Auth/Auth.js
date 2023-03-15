a405c8800f1b95491aeb02c0abc7d2207aa27a0b
Emily Sellers <emilymelindasellers@gmail.com>
2023-03-15 15:56:33 -0700

    remove unused variable in Auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink, Redirect, useParams } from 'react-router-dom';
 import { useUser } from '../../context/UserContext.js';
 import { authUser } from '../../services/auth.js';
